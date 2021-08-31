de915a733084e49d33e89ae9fea05616ef3c373c
Łukasz <l.michalik004@gmail.com>
2021-08-31 20:31:17 +0200

    Methods remind

--- Toturial.java
+++ Toturial.java
@@ -1,75 +1,111 @@
+// public class Toturial {
+//     public static void main(String[] args){
+//         // System.out.println("Hello World");
+        
+//         /*
+//         String name = "John";
+//         System.out.println(name);
+//         int myNum = 15;
+//         System.out.println(myNum);
+//         */
+        
+//         // byte myNum = 100;
+//         // System.out.println(myNum);
+
+//         // double myDouble = 9.78d;
+//         // int myInt = (int) myDouble; // Manual casting: double to int
+    
+//         // System.out.println(myDouble);   // Outputs 9.78
+//         // System.out.println(myInt);      // Outputs 9
+
+//         // int sum1 = 100 + 50;        // 150 (100 + 50)
+//         // int sum2 = sum1 + 250;      // 400 (150 + 250)
+//         // int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
+//         // String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//         // System.out.println("The length of the txt string is: " + txt.length());
+
+//         // String txt = "We are the so-called \"Vikings\" from the north.";
+
+//         // boolean isJavaFun = true;
+//         // boolean isFishTasty = false;
+//         // System.out.println(isJavaFun);     // Outputs true
+//         // System.out.println(isFishTasty);   // Outputs false
+
+//         // int time = 22;
+//         // if (time < 10) {
+//         //   System.out.println("Good morning.");
+//         // } else if (time < 20) {
+//         //   System.out.println("Good day.");
+//         // } else {
+//         //   System.out.println("Good evening.");
+//         // }
+//         // // Outputs "Good evening."
+
+//         // int day = 4;
+//         // switch (day) {
+//         //   case 1:
+//         //     System.out.println("Monday");
+//         //     break;
+//         //   case 2:
+//         //     System.out.println("Tuesday");
+//         //     break;
+//         //   case 3:
+//         //     System.out.println("Wednesday");
+//         //     break;
+//         //   case 4:
+//         //     System.out.println("Thursday");
+//         //     break;
+//         //   case 5:
+//         //     System.out.println("Friday");
+//         //     break;
+//         //   case 6:
+//         //     System.out.println("Saturday");
+//         //     break;
+//         //   case 7:
+//         //     System.out.println("Sunday");
+//         //     break;
+//         // }
+//         // // Outputs "Thursday" (day 4)
+
+//         // String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+//         // System.out.println(cars[0]);
+//         // // Outputs Volvo
+//     }
+// }
+
+// public class Toturial {
+//     static void myMethod(){
+//         System.out.println("Alo Alo");
+//     }
+//     static void myMethod1(String fname) {
+//         System.out.println(fname + " Refsnes");
+//       }
+//     public static int sum(int k) {
+//         if (k > 0) {
+//         return k + sum(k - 1);
+//         } else {
+//         return 0;
+//         }
+//     }
+//     public static void main(String[] args){
+//         myMethod();
+//         myMethod1("Ales");
+
+//         // Code here CANNOT use x
+
+//         int x = 100;
+
+//         // Code here CAN use x
+//         System.out.println(x);
+
+//         int result = sum(10);
+//         System.out.println(result);
+//     }
+// }
+
 public class Toturial {
     public static void main(String[] args){
-        // System.out.println("Hello World");
-        
-        /*
-        String name = "John";
-        System.out.println(name);
-        int myNum = 15;
-        System.out.println(myNum);
-        */
-        
-        // byte myNum = 100;
-        // System.out.println(myNum);
 
-        // double myDouble = 9.78d;
-        // int myInt = (int) myDouble; // Manual casting: double to int
-    
-        // System.out.println(myDouble);   // Outputs 9.78
-        // System.out.println(myInt);      // Outputs 9
-
-        // int sum1 = 100 + 50;        // 150 (100 + 50)
-        // int sum2 = sum1 + 250;      // 400 (150 + 250)
-        // int sum3 = sum2 + sum2;     // 800 (400 + 400)
-
-        // String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        // System.out.println("The length of the txt string is: " + txt.length());
-
-        // String txt = "We are the so-called \"Vikings\" from the north.";
-
-        // boolean isJavaFun = true;
-        // boolean isFishTasty = false;
-        // System.out.println(isJavaFun);     // Outputs true
-        // System.out.println(isFishTasty);   // Outputs false
-
-        // int time = 22;
-        // if (time < 10) {
-        //   System.out.println("Good morning.");
-        // } else if (time < 20) {
-        //   System.out.println("Good day.");
-        // } else {
-        //   System.out.println("Good evening.");
-        // }
-        // // Outputs "Good evening."
-
-        // int day = 4;
-        // switch (day) {
-        //   case 1:
-        //     System.out.println("Monday");
-        //     break;
-        //   case 2:
-        //     System.out.println("Tuesday");
-        //     break;
-        //   case 3:
-        //     System.out.println("Wednesday");
-        //     break;
-        //   case 4:
-        //     System.out.println("Thursday");
-        //     break;
-        //   case 5:
-        //     System.out.println("Friday");
-        //     break;
-        //   case 6:
-        //     System.out.println("Saturday");
-        //     break;
-        //   case 7:
-        //     System.out.println("Sunday");
-        //     break;
-        // }
-        // // Outputs "Thursday" (day 4)
-
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        System.out.println(cars[0]);
-        // Outputs Volvo
     }
 }
